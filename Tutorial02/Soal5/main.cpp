2c1971901d558cfe9b717b69291fdb91b21e5098
Staryo40 <arrrryow@gmail.com>
2025-03-14 19:10:52 +0700

    add: finish of tutorial02

--- Tutorial02/Soal5/main.cpp
+++ Tutorial02/Soal5/main.cpp
@@ -7,13 +7,16 @@
 int main()
 {
   FirePokemon* f1 = new FirePokemon("Charmander",100, 20);
-  FirePokemon f2(*f1);
+  FirePokemon* f2 = new FirePokemon(*f1);
   WaterPokemon* w1 = new WaterPokemon("Squirtle", 100, 20, 10);
   w1->heal(10);
-  WaterPokemon m2("Magikarp", 100, 20, 0);
-  m2 = *w1;
+  WaterPokemon* m2 = new WaterPokemon("Magikarp", 100, 20, 0);
+  *m2 = *w1;
   w1->attack(*f1);
   w1->attack(*f1);
+
+  delete f1;
+  delete w1;
 
   return 0;
 }
